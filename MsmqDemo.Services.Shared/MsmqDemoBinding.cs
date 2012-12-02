032234531c73e533545d9069a7dc61cbcf88097e
Paul R. Bahler <pbahler@igsenergy.com>
2012-12-02 11:12:40 -0500

    removed binding config

--- MsmqDemo.Services.Shared/MsmqDemoBinding.cs
+++ MsmqDemo.Services.Shared/MsmqDemoBinding.cs
@@ -6,19 +6,5 @@ namespace MsmqDemo.Services.Shared
     public class MsmqDemoBinding
         : NetMsmqBinding
     {
-        public MsmqDemoBinding()
-        {
-            ReceiveRetryCount = 1;
-            MaxRetryCycles = 5;
-            RetryCycleDelay = TimeSpan.FromMinutes(5);
-            
-            TimeToLive = TimeSpan.FromDays(7);
-
-            ReceiveErrorHandling = ReceiveErrorHandling.Move;
-
-            Security.Mode = NetMsmqSecurityMode.None;
-            Security.Message.ClientCredentialType = MessageCredentialType.None;
-            Security.Transport.MsmqAuthenticationMode = MsmqAuthenticationMode.None;
-        }
     }
 }
